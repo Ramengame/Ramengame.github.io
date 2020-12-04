a4dc20af297f57a9a15f5ca86235bcd958bb034e
Ramengame <74910182+Ramengame@users.noreply.github.com>
2020-12-04 07:09:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-var money = 0
-var ramen = 0
-
-function getRamen (){
-    if (money >= 5.50) {
-    ramen = ramen + 1;
-    money = money - ramen;
-    };
+var ramen = 0;
+var totalRamen = 0;
+var money = 0;
+var totalMoney = 0;
+var brainPower = 0;
+    
+function getMoney(number){
+      money = money + number;
+      document.getElementById("money").innerHTML = money;
 };
 
-function getMoney(number) {
-    money = money + 1;
+function getRamen(){
+    var ramenCost = 5.75;
+    if(money >= ramenCost){
+      ramen = ramen + 1;
+    	money = money - ramenCost;
+      document.getElementById('ramen').innerHTML = ramen;
+      document.getElementById('money').innerHTML = money;
+      eatRamenText();
+    } else {
+    alert("The store clerk looks at you incredously. You know you need money to get Ramen, Right?");
+    }
+};
+
+function ramenStats(){
+    totalRamen = totalRamen + 1;
+    document.getElementById('totalRamen').innerHTML = totalRamen;
+};
+  
+function eatRamen(){
+    if(ramen >= 1){
+    brainPower = brainPower + 1;
+    } else{
+    alert("You can't eat any Ramen because you don't HAVE any Ramen. This is unacceptable >:(");
+    }
 };
