f8184830156fbc67acbf3decc070722be8f26e30
Ramengame <74910182+Ramengame@users.noreply.github.com>
2020-12-04 12:34:16 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,6 @@ var money = 0;
 var totalMoney = 0;
 var brainPower = 0;
     
-var noMoney = "The store clerk looks at you incredously. You know you need money to get Ramen, Right?"
-
 function getMoney(number){
       money = money + number;
       document.getElementById("money").innerHTML = money;
@@ -14,21 +12,6 @@ function getMoney(number){
 function buyRamenButton(){
     getRamen();
 };
-    
-function getRamen(){
-    var ramenCost = 0.5;
-    if(money >= ramenCost){
-      ramen = ramen + 1;
-    	money = money - ramenCost;
-      document.getElementById('ramen').innerHTML = ramen;
-      document.getElementById('money').innerHTML = money;
-      eatRamenText();
-      ramenStats();
-    } else {
-    alert("The store clerk looks at you incredously. You know you need money to get Ramen, Right?");
-    }
-};
-
 function samsRamen(){
     var samsCost = 8.28;
     if(money >= samsCost){
@@ -38,6 +21,17 @@ function samsRamen(){
     } else {
 	alert(noMoney);
     };
+};
+function getRamen(){
+    var ramenCost = 0.5;
+    if(money >= ramenCost){
+      ramen = ramen + 1;
+    	money = money - ramenCost;
+      document.getElementById('ramen').innerHTML = ramen;
+      document.getElementById('money').innerHTML = money;
+    } else {
+    alert("The store clerk looks at you incredously. You know you need money to get Ramen, Right?");
+    }
 };
 
 function ramenStats(){
@@ -55,17 +49,6 @@ function eatRamen(){
     alert("You can't eat any Ramen because you don't HAVE any Ramen. This is unacceptable >:(");
     }
 };
-
-function brainUpgrade(){
-	if(brainPower = 1){
-	alert("You train your cat to get you money. +0.5 money/sec");
-	} else if(brainPower = 2){
-	alert("You ask your roommates to look for money, but they take half :(. +0.25 money/sec");
-	} else {
-	alert("another bowl of yummy Ramen");
-	};
-};
-	
 
 window.setInterval(function(){
     
