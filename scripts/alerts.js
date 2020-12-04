7eff2e2c4cc6e92eb9522ea6bf2f1b57f32ace78
Ramengame <74910182+Ramengame@users.noreply.github.com>
2020-12-04 10:03:18 -0600

    Create alerts.js

---
+++ scripts/alerts.js
@@ -0,0 +1,7 @@
+function brainWords(){
+    if (brainPower = 1){
+    alert("You figure you can get money from places besides the couch. +1 money per click"
+    } else if (brainPower = 2){
+    alert("What if we acctually went to work. Thats means more money. That means more Ramen :)  +2 money per click")
+    }
+    };
